46e5e3b07e28faa03a92810aa07d0a0e4a9e6525
emperor-akashi-4 <64988307+emperor-akashi-4@users.noreply.github.com>
2020-05-08 02:48:39 +0530

    Add files via upload

---
+++ userbot/plugins/l.py
@@ -0,0 +1,37 @@
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.3
+
+    animation_ttl = range(0, 10)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "l":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+        
+            "[I AM A BOY | I AM A BOY\nI AM A BOY | I AM A BOY\nCLICK HERE TO JOIN NUDES CHANNEL\nCLICK HERE TO JOIN NUDES CHANNEL\nCLICK HERE TO JOIN NUDES CHANNEL\nCLICK HERE TO JOIN NUDES CHANNEL\nCLICK HERE TO JOIN NUDES CHANNEL\nCLICK HERE TO JOIN NUDES CHANNEL\nCLICK HERE TO JOIN NUDES CHANNEL\nCLICK HERE TO JOIN NUDES CHANNEL\nCLICK HERE TO JOIN NUDES CHANNEL\nCLICK HERE TO JOIN NUDES CHANNEL\nCLICK HERE TO JOIN NUDES CHANNEL\nCLICK HERE TO JOIN NUDES CHANNEL\nCLICK HERE TO JOIN NUDES CHANNEL\nCLICK HERE TO JOIN NUDES CHANNEL\nCLICK HERE TO JOIN NUDES CHANNEL](https://t.me/indiancollections)"
+
+ ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 10])
